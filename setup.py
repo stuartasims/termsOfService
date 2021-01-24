d987d0be17dc4c86ad411d7c9b105e8864e6cd39
stuartasims <stuart.a.sims@gmail.com>
2021-01-23 22:16:16 -0700

    Initial Upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploratory Data Analysis on the Terms of service and other legal documents from a variety of companies.',
+    author='Stuart Sims and Caitlyn Van Heest',
+    license='MIT',
+)
